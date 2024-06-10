cd3af1376004a9afdfd1cb690e490deb962ad9df
Srishty M <srishtymangutte@gmail.com>
2024-06-10 16:41:39 +0530

    feat: added users and wallets modulkes

--- project/src/app.module.ts
+++ project/src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { WalletsModule } from './wallets/wallets.module';
+
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UsersModule,
+    WalletsModule,
+    WalletsModule,
+  ],
 })
 export class AppModule {}
